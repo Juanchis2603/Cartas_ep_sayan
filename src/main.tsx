3f0c34a38b01ebb06e31efb569bf3e014afe071b
Juanchis2603 <juanmanu2655@gmail.com>
2026-02-21 12:07:18 -0400

    pasaron cositas
    arregle lo d el vercel

---
+++ src/main.tsx
@@ -0,0 +1,16 @@
+import { createRoot } from 'react-dom/client'
+import App from './App.tsx'
+
+import { BrowserRouter,Route, Routes } from 'react-router';
+
+createRoot(document.getElementById('root')!).render(
+  <BrowserRouter>
+
+   <Routes>
+
+    <Route path='/' element={<App/>}/>
+
+   </Routes>
+
+  </BrowserRouter>
+)
