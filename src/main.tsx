3c7b38ee8530f01298b239cc05586cf12a156040
Juanchis2603 <juanmanu2655@gmail.com>
2026-03-21 12:22:19 -0400

    cositas

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,11 @@
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import App from './App'
-import CrearCartaPage from './Screens/CrearCartaPage'
-import DetalleCartaPage from './Screens/DetalleCartaPage'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/" element={<App />}>
-        
-        <Route path="crear" element={<CrearCartaPage />} />
-        <Route path="card/:id" element={<DetalleCartaPage />} />
+      <Route path="/:id" element={<App />}>
       </Route>
     </Routes>
   </BrowserRouter>
