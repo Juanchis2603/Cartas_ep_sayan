9973ae2516e7d1297eec0484b908dcf2fc9055fa
Juanchis2603 <juanmanu2655@gmail.com>
2026-03-28 11:43:30 -0400

    cositas css

--- src/main.tsx
+++ src/main.tsx
@@ -6,6 +6,10 @@ createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <Routes>
       <Route path="/" element={<App/>}></Route>
+      <Route path="/mazo" element={<App/>}></Route>
+      <Route path="/crear-carta" element={<App/>}></Route>
+      <Route path="/detalle/:id" element={<App/>}></Route>
+      <Route path="/editar-carta/:id" element={<App/>}></Route>
     </Routes>
   </BrowserRouter>
 )
